3816ef5b312a3979ef92dfabefbe125de7e7675e
Kustaa Huhtala <kustaa.huhtala@gofore.com>
2016-03-11 11:07:06 +0200

    test

--- tennis/src/main/java/com/pyramid/tennis/Game.java
+++ tennis/src/main/java/com/pyramid/tennis/Game.java
@@ -3,24 +3,42 @@ package com.pyramid.tennis;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Game
-{
-    private String result = "love - love";
+public class Game {
+	private static final String PLAYER1 = "player1";
 
-    private Map<Integer, String> map = new HashMap<Integer, String>();
+	private String result = "love - love";
 
-    public String getPoints() {
-        return result;
-    }
+	private static final String PLAYER_1 = PLAYER1;
+	
+	private Map<Integer, String> map = new HashMap<Integer, String>();
+	{
+		map.put(0, "love");
+		map.put(1, "fifteen");
+		map.put(2, "thirty");
+		map.put(3, "forty");
+	}
 
-    public void addPoint(String string) {
-        if ("player1".equals(string)) {
-            result = "fifteen - love";
-        }
-        else if ("player2".equals(string)) {
-            result = "love - fifteen";
-        }
-    }
+	private Map<String, Integer> scores;
 
+	public Game() {
+		scores = new HashMap<String, Integer>();
+		scores.put(PLAYER1, 0);
+		scores.put("player2", 0);
+	}
+
+	public String getPoints() {
+		return result;
+	}
+
+	public void addPoint(String string) {
+		if (PLAYER1.equals(string)) {
+			int score = scores.get(PLAYER1).intValue() + 1;
+			scores.put(PLAYER1, score);
+			
+			result = map.get(1) + " - " + map.get(0);
+		} else if ("player2".equals(string)) {
+			result = map.get(0) + " - " + map.get(1);
+		}
+	}
 
 }
