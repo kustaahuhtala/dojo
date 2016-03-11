19dde3f20d2f109f3a4d4c28708d7cd0fc6e1bbd
Lasse Pirinen <lassemon.pirinen@gmail.com>
2016-03-11 11:28:39 +0200

    lasses part 4

--- tennis/src/main/java/com/pyramid/tennis/Game.java
+++ tennis/src/main/java/com/pyramid/tennis/Game.java
@@ -4,36 +4,37 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Game {
-	private static final String PLAYER2 = "player2";
+    private static final String PLAYER2 = "player2";
 
-	private static final String PLAYER1 = "player1";
+    private static final String PLAYER1 = "player1";
 
-	private Map<Integer, String> map;
+    private Map<Integer, String> map;
 
-	private Map<String, Integer> scores;
+    private Map<String, Integer> scores;
 
-	public Game() {
-		scores = new HashMap<String, Integer>();
-		scores.put(PLAYER1, 0);
-		scores.put(PLAYER2, 0);
+    public Game() {
+        scores = new HashMap<String, Integer>();
+        scores.put(PLAYER1, 0);
+        scores.put(PLAYER2, 0);
 
-		map = new HashMap<Integer, String>();
-		map.put(0, "love");
-		map.put(1, "fifteen");
-		map.put(2, "thirty");
-		map.put(3, "forty");
-	}
+        map = new HashMap<Integer, String>();
+        map.put(0, "love");
+        map.put(1, "fifteen");
+        map.put(2, "thirty");
+        map.put(3, "forty");
+    }
 
-	public String getPoints() {
-		if (scores.get(PLAYER1).equals(scores.get(PLAYER2)) && scores.) {
-			
-		}
-		return map.get(scores.get(PLAYER1)) + " - " + map.get(scores.get(PLAYER2));
-	}
+    public String getPoints() {
+        String score = map.get(scores.get(PLAYER1)) + " - " + map.get(scores.get(PLAYER2));
+        if (scores.get(PLAYER1).equals(scores.get(PLAYER2)) && scores.get(PLAYER1) > 3) {
+            return score + " (deuce)";
+        }
+        return score;
+    }
 
-	public void addPoint(String player) {
-		int score = scores.get(player).intValue() + 1;
-		scores.put(player, score);
-	}
+    public void addPoint(String player) {
+        int score = scores.get(player).intValue() + 1;
+        scores.put(player, score);
+    }
 
 }
--- tennis/src/test/java/com/pyramid/tennis/AppTest.java
+++ tennis/src/test/java/com/pyramid/tennis/AppTest.java
@@ -7,51 +7,51 @@ import org.junit.Test;
 
 public class AppTest {
 
-	Game game;
+    Game game;
 
-	@Before
-	public void init() {
-		game = new Game();
-	}
+    @Before
+    public void init() {
+        game = new Game();
+    }
 
-	@Test
-	public void initGame() {
-		assertEquals("love - love", new Game().getPoints());
-	}
+    @Test
+    public void initGame() {
+        assertEquals("love - love", new Game().getPoints());
+    }
 
-	@Test
-	public void addPoint() {
-		Game game = new Game();
-		game.addPoint("player1");
-		assertEquals("fifteen - love", game.getPoints());
-	}
+    @Test
+    public void addPoint() {
+        Game game = new Game();
+        game.addPoint("player1");
+        assertEquals("fifteen - love", game.getPoints());
+    }
 
-	@Test
-	public void addPointToPlayer2() {
-		Game game = new Game();
-		game.addPoint("player2");
-		assertEquals("love - fifteen", game.getPoints());
-	}
+    @Test
+    public void addPointToPlayer2() {
+        Game game = new Game();
+        game.addPoint("player2");
+        assertEquals("love - fifteen", game.getPoints());
+    }
 
-	@Test
-	public void addSecondPointToPlayer2() {
-		game.addPoint("player2");
-		game.addPoint("player2");
-		assertEquals("love - thirty", game.getPoints());
-	}
+    @Test
+    public void addSecondPointToPlayer2() {
+        game.addPoint("player2");
+        game.addPoint("player2");
+        assertEquals("love - thirty", game.getPoints());
+    }
 
-	@Test
-	public void addSecondPointToPlayers() {
-		game.addPoint("player2");
-		game.addPoint("player1");
-		assertEquals("fifteen - fifteen", game.getPoints());
-		game.addPoint("player2");
-		game.addPoint("player1");
-		assertEquals("thirty - thirty", game.getPoints());
-		game.addPoint("player1");
-		assertEquals("forty - thirty", game.getPoints());
-		game.addPoint("player2");
-		assertEquals("deuce", game.getPoints());
-	}
+    @Test
+    public void addPointsToPlayers() {
+        game.addPoint("player2");
+        game.addPoint("player1");
+        assertEquals("fifteen - fifteen", game.getPoints());
+        game.addPoint("player2");
+        game.addPoint("player1");
+        assertEquals("thirty - thirty (deuce)", game.getPoints());
+        game.addPoint("player1");
+        assertEquals("forty - thirty", game.getPoints());
+        game.addPoint("player2");
+        assertEquals("forty - forty (deuce)", game.getPoints());
+    }
 
 }
