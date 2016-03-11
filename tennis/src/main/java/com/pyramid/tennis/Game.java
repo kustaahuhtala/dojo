532b2485a5fb7f5c793c33b268ba499bbd182110
Lasse Pirinen <lassemon.pirinen@gmail.com>
2016-03-11 10:56:31 +0200

    Add lasses part 2

--- tennis/src/main/java/com/pyramid/tennis/Game.java
+++ tennis/src/main/java/com/pyramid/tennis/Game.java
@@ -1,18 +1,26 @@
 package com.pyramid.tennis;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Game
 {
-	private String result = "love - love";
+    private String result = "love - love";
+
+    private Map<Integer, String> map = new HashMap<Integer, String>();
+
     public String getPoints() {
         return result;
     }
 
-	public void addPoint(String string) {
-		if ("player1".equals(string))
-		result = "fifteen - love";
-		else if("player2".equals(string))
-			result = "love - fifteen";
-	}
+    public void addPoint(String string) {
+        if ("player1".equals(string)) {
+            result = "fifteen - love";
+        }
+        else if ("player2".equals(string)) {
+            result = "love - fifteen";
+        }
+    }
 
-	
+
 }
--- tennis/src/test/java/com/pyramid/tennis/AppTest.java
+++ tennis/src/test/java/com/pyramid/tennis/AppTest.java
@@ -7,34 +7,36 @@ import org.junit.Test;
 
 public class AppTest {
 
-	Game game;
-	
-	@Before
-	public void init() {
-		game = new Game();
-	}
-	
-	@Test
-	public void initGame() {
-		assertEquals("love - love", new Game().getPoints());
-	}
+    Game game;
 
-	@Test
-	public void addPoint() {
-		Game game = new Game();
-		game.addPoint("player1");
-		assertEquals("fifteen - love", game.getPoints());
-	}
+    @Before
+    public void init() {
+        game = new Game();
+    }
 
-	@Test
-	public void addPointToPlayer2() {
-		Game game = new Game();
-		game.addPoint("player2");
-		assertEquals("love - fifteen", game.getPoints());
-	}
-	
-	@Test
-	public void addSecondPointToPlayer2() {
-		game.addpoi
-	}
+    @Test
+    public void initGame() {
+        assertEquals("love - love", new Game().getPoints());
+    }
+
+    @Test
+    public void addPoint() {
+        Game game = new Game();
+        game.addPoint("player1");
+        assertEquals("fifteen - love", game.getPoints());
+    }
+
+    @Test
+    public void addPointToPlayer2() {
+        Game game = new Game();
+        game.addPoint("player2");
+        assertEquals("love - fifteen", game.getPoints());
+    }
+
+    @Test
+    public void addSecondPointToPlayer2() {
+        game.addPoint("player2");
+        game.addPoint("player2");
+        assertEquals("love - thirty", game.getPoints());
+    }
 }
